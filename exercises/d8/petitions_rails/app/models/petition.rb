706e16e66de1a5123e4d06b5e2a3f7e0529ab922
RachelKnapp <rachelkknapp@gmail.com>
2015-06-10 21:00:23 +0200

    final petitions

--- exercises/d8/petitions_rails/app/models/petition.rb
+++ exercises/d8/petitions_rails/app/models/petition.rb
@@ -1,8 +1,8 @@
 class Petition < ActiveRecord::Base
 	def self.get_petitions
-		response = RestClient.get "https://api.whitehouse.gov/v1/petitions.json?limit=10&offset=0"
+		response = RestClient.get "https://api.whitehouse.gov/v1/petitions.json"
 		parsed_response = JSON.parse(response)
-		petitions = parsed_response[results]
-		petitions.map! {|petition| petition["title"]}
+		petitions = parsed_response["results"]
+		return petitions
 	end
 end
