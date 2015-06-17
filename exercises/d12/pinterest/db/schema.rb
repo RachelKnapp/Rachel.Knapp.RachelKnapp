c47a1e4e14557f7f2d89456f6abdbf87aaa3d741
RachelKnapp <rachelkknapp@gmail.com>
2015-06-17 14:50:22 +0200

    in process of adding users

--- exercises/d12/pinterest/db/schema.rb
+++ exercises/d12/pinterest/db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150616200347) do
+ActiveRecord::Schema.define(version: 20150617124147) do
 
   create_table "items", force: :cascade do |t|
     t.string  "body"
     t.string  "url"
     t.integer "user_id"
     t.string  "category"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "category"
   end
 
 end
